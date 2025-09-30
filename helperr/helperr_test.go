ad0f1b5bb6d8d91554108a31e7ef1d4bb2761dc3
Marin Nozhchev <murfffi@gmail.com>
2025-09-30 20:10:14 +0300

    fixup

--- helperr/helperr_test.go
+++ helperr/helperr_test.go
@@ -6,8 +6,25 @@ import (
 	"github.com/murfffi/gorich/helperr"
 )
 
+type mockCloser struct {
+	closeCalled bool
+}
+
+func (m *mockCloser) Close() error {
+	m.closeCalled = true
+	return nil
+}
+
 func TestCloseQuietly(t *testing.T) {
 	t.Run("nil", func(t *testing.T) {
 		helperr.CloseQuietly(nil)
+	})
+
+	t.Run("non-nil", func(t *testing.T) {
+		mc := &mockCloser{}
+		helperr.CloseQuietly(mc)
+		if !mc.closeCalled {
+			t.Error("expected Close() to be called, but it was not")
+		}
 	})
 }
